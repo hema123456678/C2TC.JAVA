7a405a56c13c2616d42b0d950596946850b2de4a
Admin <Admin@DESKTOP-RC4PBRV>
2022-05-09 19:20:27 -0800

    FIRST COMMIT

--- recursion/src/recursion/recursion1.java
+++ recursion/src/recursion/recursion1.java
@@ -5,7 +5,14 @@ public class recursion1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method 
-
+		int number = 123456,reverse = 0;
+		while(number !=0)
+		{
+			int remainder = number % 10;
+			reverse = reverse *10+ remainder;
+			number = number/10;
+		}
+		System.out.println("The reverse of the given number is:" +reverse);
 		
 
 	}
